a706e7fe804f10644244bb99fd5775a83f8cdea6
Mickey Shaughnessy <mickeyshaughnessy@gmail.com>
2023-07-11 09:45:18 -0600

    add files

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Because who doesn't love a good Mars farming joke?
+echo "Preparing to farm on Mars... hope you brought your space overalls."
+
+# Check for Python 3.8 or higher
+if ! python3 --version | awk -F. '{if ($2<8) exit 1}'; then
+    echo "Python 3.8 or higher is required. Please upgrade and try again."
+    exit 1
+fi
+
+# Check for git
+if ! command -v git &> /dev/null; then
+    echo "Git is not installed. Please install it and try again."
+    exit 1
+fi
+
+# Clone the repository
+echo "Pulling down the Martian blueprints... I mean, code."
+git clone https://github.com/mickeyshaughnessy/MarsFarming.git mars-farm-sim
+
+# Install necessary Python libraries
+echo "Installing interplanetary farming tools... I mean, Python libraries."
+python3 -m pip install pygame pygame_gui pygame_sockets
+
+echo "Done! Welcome to your new Martian farm. Remember, in space, no one can hear you reap."
+
---
+++ main.py
@@ -0,0 +1,52 @@
+import sys
+import pygame
+import pygame_gui
+
+# Initialize Pygame
+pygame.init()
+
+# Set up some constants
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+FPS = 60
+
+# Create the game window
+window_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+# Load the map image
+mars_map = pygame.image.load('assets/images/mars_map.png')
+
+# Create a GUI manager
+manager = pygame.gui.UIManager((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+# Main game loop
+while True:
+    # Event handling
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+        # Pass the event to the GUI manager
+        manager.process_events(event)
+
+        # Check for mouse click
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # Open a dialog window
+            pygame_gui.windows.UIConfirmationDialog(rect=(100, 100, 400, 200),
+                                                    manager=manager,
+                                                    window_title='Robot Control',
+                                                    action_long_desc='Interact with the robot control software.',
+                                                    action_short_name='OK',
+                                                    blocking=True)
+
+    # Update the GUI manager
+    manager.update(FPS / 1000.0)
+
+    # Draw everything
+    window_surface.blit(mars_map, (0, 0))  # Draw the map
+    manager.draw_ui(window_surface)
+
+    # Flip the display
+    pygame.display.flip()
+
